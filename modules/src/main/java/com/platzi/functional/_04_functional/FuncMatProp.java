0db7f444294e0967c77236ea83a8a04e2f6405f6
AlejandroManzano <alex.batt99@gmail.com>
2020-05-26 23:54:54 -0500

    Se agregan uso y ejecucion de Funciones y Predicados

--- modules/src/main/java/com/platzi/functional/_04_functional/FuncMatProp.java
+++ modules/src/main/java/com/platzi/functional/_04_functional/FuncMatProp.java
@@ -1,6 +1,8 @@
 package com.platzi.functional._04_functional;
 
+import java.sql.SQLOutput;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 /**
  * @author Alex Manzano
@@ -12,10 +14,35 @@ public class FuncMatProp {
         Function<Integer, Integer> cuadrado = new Function<Integer, Integer>() {
             @Override
             public Integer apply(Integer x) {
-                return x*x;
+                return x * x;
             }
         };
 
         System.out.println(cuadrado.apply(12));
+        /*Abajo dice: entra x, devuelve un boolean el modulo de x es igual a 1(aprox)*/
+        Function<Integer, Boolean> esImpar = x -> x % 2 == 1;
+        System.out.println("Resultado con Func: " + esImpar.apply(4));
+        //En este caso Predicate hace lo mismo que la funcion de arriba de forma automatica
+        //pero solo es para obtener estos boolean, cualquier otro tipo de resultado, sera de buscarlo
+        //con Function
+        Predicate<Integer> estaPar = x -> x % 2 == 0;
+        estaPar.test(4);
+        System.out.println("Resultado con Pred: " + estaPar.test(4));
+
+        Predicate<Estudiante> aprobado = estudiante -> estudiante.getCalificacion() >= 6.0;
+        System.out.println("Aprobo el estudiante: " + aprobado.test(new Estudiante(5.0)));
+
+    }
+
+    public static class Estudiante{
+        private double calificacion;
+
+        public Estudiante(double calificacion) {
+            this.calificacion = calificacion;
+        }
+
+        public double getCalificacion() {
+            return calificacion;
+        }
     }
 }
